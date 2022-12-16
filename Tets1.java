b32f7eb0f4fdc308dbbe4979d92bef2c9a13843a
Saurabh8006 <saurabhchavan8006@gmail.com>
2022-12-16 19:37:15 +0530

    Create Tets1.java

---
+++ Tets1.java
@@ -0,0 +1,11 @@
+package com.generalPractice;
+
+public class Tets1 {
+			public static void main(String[] args) {
+				int i =1;
+				for(;;)
+				{
+					System.out.println("Hi");
+				}	
+			}
+}
