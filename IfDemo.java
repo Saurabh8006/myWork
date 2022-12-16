11b0b92835af9da53302ca8afa2ac0eb077d13f1
Saurabh8006 <saurabhchavan8006@gmail.com>
2022-12-16 19:36:28 +0530

    Create IfDemo.java

---
+++ IfDemo.java
@@ -0,0 +1,17 @@
+package com.generalPractice;
+
+public class IfDemo {
+
+	public static void main(String[] args) {
+		int no1 = 10;
+		int no2 = 20;
+		if(no1==no2)
+		{
+			no2 =30;
+			System.out.println(no2);
+		}
+		System.out.println("end the conditional statement");
+
+	}
+
+}
